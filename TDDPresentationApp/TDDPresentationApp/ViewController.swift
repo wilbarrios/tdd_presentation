8842d6fd441c7df08e61f44c9e67af35913da31d
Wilmer Barrios <wilmer@grainchain.io>
2021-03-02 22:59:45 -0600

    Project initialization added

---
+++ TDDPresentationApp/TDDPresentationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDDPresentationApp
+//
+//  Created by Wilmer Barrios on 02/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TDDPresentationApp/TDDPresentationAppTests/TDDPresentationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TDDPresentationAppTests.swift
+//  TDDPresentationAppTests
+//
+//  Created by Wilmer Barrios on 02/03/21.
+//
+
+import XCTest
+@testable import TDDPresentationApp
+
+class TDDPresentationAppTests: XCTestCase {
+
+    func test_init() {
+//        XCTFail()
+    }
+
+}
